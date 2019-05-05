6de6e7b4bf522616f555c267dbcca9860983e05a
Carlos Marques <carlos_marques@email.com>
2019-05-05 18:51:34 -0300

    Atualizando a base do jogo
    Ponto de parada: https://youtu.be/x6imuef9Q4U?t=1111

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,4 +1,3 @@
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
@@ -7,25 +6,58 @@ public class Jogador extends Rectangle
 	private static final long serialVersionUID = 1L;
 	
 	public boolean cima, direita, baixo, esquerda;
-	public int velocidade = 4;
+	public int velocidade = 2;
 
 	public Jogador(int x, int y)
 	{
-		setBounds(x, y, 32, 32); // Definir tamanho e posição do retângulo
+		setBounds(x,y, 32,32); // Definir tamanho e posição do retângulo
+	}
+	
+	private boolean podeMover(int x, int y)
+	// Checamos se o próximo ladrilho está vazio para podermos mover
+	{
+		Rectangle colisao = new Rectangle(x,y, width,height); // Retângulo invisível à frente do player, para checar a colisão
+		
+		for (int i=0; i < Pacman.mapa.ladrilhos.length; i++)
+			for (int j=0; j < Pacman.mapa.ladrilhos[0].length; j++)
+				if (Pacman.mapa.ladrilhos[i][j] != null)
+					if (colisao.intersects(Pacman.mapa.ladrilhos[i][j]))
+						return false; // Se colidir com algum dos ladrilhos do mapa, o player não pode mover
+		
+		return true;
 	}
 	
 	public void tick()
 	{
 		// Movimentação do jogador
-		if (cima) y -= velocidade;
-		if (direita) x += velocidade;
-		if (baixo) y += velocidade;
-		if (esquerda) x -= velocidade;
+		if (cima && podeMover(x,y-velocidade))
+			y -= velocidade;
+		if (direita && podeMover(x+velocidade,y))
+			x += velocidade;
+		if (baixo && podeMover(x,y+velocidade))
+			y += velocidade;
+		if (esquerda && podeMover(x-velocidade,y))
+			x -= velocidade;
+		
+		for (int i=0; i < Pacman.mapa.pastilhas.size(); i++)
+		{
+			if (intersects(Pacman.mapa.pastilhas.get(i))) // Se colidir com uma pastilha...
+			{
+				Pacman.mapa.pastilhas.remove(i); // Deleta a pastilha
+				break;
+			}
+		}
+		
+		if (Pacman.mapa.pastilhas.size() == 0) // Se o jogador comer todas as pastilhas, é vencedor
+		{
+			Pacman.jogador = new Jogador(0,0);
+			Pacman.mapa = new Mapa("/mapas/mapa1.png");
+			return;
+		}
 	}
 	
 	public void render(Graphics graficos)
 	{
-		graficos.setColor(Color.yellow); // Define a cor do jogador
-		graficos.fillRect(x, y, width, height); // Define onde renderizar o jogador
+		graficos.drawImage(Pacman.malha.getSprite(0,0), x,y, width,height, null);
 	}
 }
