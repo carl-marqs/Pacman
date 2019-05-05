eab8b602be07281663d471743165390785b53621
Carlos Marques <carlos_marques@email.com>
2019-05-05 18:53:03 -0300

    Atualizando a base do jogo
    Ponto de parada: https://youtu.be/x6imuef9Q4U?t=1111

---
+++ src/Ladrilho.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Ladrilho extends Rectangle
+{
+	private static final long serialVersionUID = 1L;
+
+	public Ladrilho(int x, int y)
+	{
+		setBounds(x,y, 32,32); // Define o tamanho e posição do retângulo
+	}
+	
+	public void render(Graphics graficos)
+	{
+		graficos.setColor(new Color(0,0,255)); // Definir a cor de fundo
+		graficos.fillRect(x,y, width,height); // Define onde renderizar o ladrilho
+	}
+}
